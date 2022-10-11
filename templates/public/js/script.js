6c2073229af9a3374f73720a21454392e3cafff5
jabbar-dev <rashdijabbar@gmail.com>
2022-10-11 21:12:07 +0500

    New Changed Files

--- templates/public/js/script.js
+++ templates/public/js/script.js
@@ -1,9 +1,13 @@
 const video = document.getElementById('videoInput')
+let detected = 0;
+let detectedF = [];
+let count = 0;
 Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.ssdMobilenetv1.loadFromUri('/models') //heavier/accurate version of tiny face detector
 ]).then(start)
+
 
 function start() {
     document.body.append('Models Loaded')
@@ -47,6 +51,23 @@ async function recognizeFaces() {
                 return faceMatcher.findBestMatch(d.descriptor)
             })
             results.forEach( (result, i) => {
+
+                console.log(result.label)
+                if((!detectedF.includes(result.label)) && (result.label != 'unknown')) {
+
+                    let hold = result.label
+                    if(result.label==hold){
+                        count+=1
+                    }
+                    else{
+                        count = 0;
+                    }
+                    if(count>100){
+                    detectedF.push(result.label)
+                    count=0;}
+                }
+                console.log(detectedF)
+
                 const box = resizedDetections[i].detection.box
                 const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
                 drawBox.draw(canvas)
@@ -59,9 +80,31 @@ async function recognizeFaces() {
     })
 }
 
+// Example POST method implementation:
+async function uploadResult() {
+    // Default options are marked with *
+    const response = await fetch("http://localhost:3002/api/analytic", {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'no-cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({"testID":"999", "count":012}) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  
+
+
 
 function loadLabeledImages() {
-    const labels = ['AbdulJabbar','Arslan','Black Widow', 'Captain America', 'Hawkeye' , 'Jim Rhodes', 'Tony Stark', 'Thor', 'Captain Marvel']
+    const labels = ['AbdulJabbar','Arslan','DrAhsanullah','DrMuhammadHussain']
     // const labels = [] // for WebCam
     return Promise.all(
         labels.map(async (label)=>{
@@ -72,7 +115,7 @@ function loadLabeledImages() {
                 console.log(label + i + JSON.stringify(detections))
                 descriptions.push(detections.descriptor)
             }
-            document.body.append(label+' Faces Loaded | ')
+            // document.body.append(label+' Faces Loaded | ')
             return new faceapi.LabeledFaceDescriptors(label, descriptions)
         })
     )
--- templates/src/app.js
+++ templates/src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -7,6 +8,12 @@ const port = process.env.PORT || 3000
 const basePath = path.join(__dirname, '../public')
 
 app.use(express.static(basePath))
+var corsOptions = {
+    origin: "http://localhost:3002"
+  };
+  
+  app.use(cors(corsOptions));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(basePath, 'one.html'))
